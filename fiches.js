e80fdb767239b0e5dd9e72008e0e1c435f3a38a6
SpectraSolar0 <leo040016@gmail.com>
2025-07-01 16:38:58 +0100

    Add files via upload

---
+++ fiches.js
@@ -0,0 +1,15 @@
+module.exports = {
+  Jackson: { nom: 'Jackson', prenom: 'Rick', age: 30, cheveux: 'Brun', yeux: 'Bleu', numero: '555-1111', braquages: 5, ventes: 12, avertissements: 1 },
+  Lina: { nom: 'Lina', prenom: 'Martinez', age: 25, cheveux: 'Blond', yeux: 'Vert', numero: '555-1112', braquages: 2, ventes: 5, avertissements: 0 },
+  Carlos: { nom: 'Carlos', prenom: 'Gomez', age: 29, cheveux: 'Noir', yeux: 'Marron', numero: '555-1113', braquages: 3, ventes: 8, avertissements: 1 },
+  Miguel: { nom: 'Miguel', prenom: 'Ramos', age: 32, cheveux: 'Châtain', yeux: 'Gris', numero: '555-1114', braquages: 6, ventes: 15, avertissements: 2 },
+  Lucia: { nom: 'Lucia', prenom: 'Lopez', age: 27, cheveux: 'Brun', yeux: 'Bleu', numero: '555-1115', braquages: 1, ventes: 4, avertissements: 0 },
+  Antonio: { nom: 'Antonio', prenom: 'Perez', age: 34, cheveux: 'Noir', yeux: 'Noir', numero: '555-1116', braquages: 7, ventes: 20, avertissements: 3 },
+  Rosa: { nom: 'Rosa', prenom: 'Fernandez', age: 26, cheveux: 'Rouge', yeux: 'Vert', numero: '555-1117', braquages: 2, ventes: 6, avertissements: 1 },
+  Javier: { nom: 'Javier', prenom: 'Castro', age: 31, cheveux: 'Noir', yeux: 'Bleu', numero: '555-1118', braquages: 5, ventes: 10, avertissements: 1 },
+  Angel: { nom: 'Saucedo', prenom: 'Angel', age: 31, cheveux: 'Marron', yeux: 'Bleu', numero: '(602) 726 7178', braquages: 0, ventes: 0, avertissements: 0 },
+  Tony: { nom: 'Tony', prenom: 'Smith', age: 28, cheveux: 'Noir', yeux: 'Marron', numero: '555-2222', braquages: 3, ventes: 7, avertissements: 0 },
+  Ricky: { nom: 'Ricky', prenom: 'Jones', age: 33, cheveux: 'Châtain', yeux: 'Vert', numero: '555-2223', braquages: 4, ventes: 9, avertissements: 2 },
+  Luis: { nom: 'Luis', prenom: 'Garcia', age: 29, cheveux: 'Noir', yeux: 'Gris', numero: '555-2224', braquages: 6, ventes: 14, avertissements: 1 },
+  Pablo: { nom: 'Pablo', prenom: 'Diaz', age: 35, cheveux: 'Brun', yeux: 'Bleu', numero: '555-2225', braquages: 8, ventes: 18, avertissements: 3 }
+};
