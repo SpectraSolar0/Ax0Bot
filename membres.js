f29f9f313c25b447928afcce43f09a94211a2217
SpectraSolar0 <leo040016@gmail.com>
2025-07-01 16:32:49 +0100

    Add files via upload

---
+++ membres.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'Prospect': ['Jackson', 'Lina'],
+  'Miembro': ['Carlos', 'Miguel'],
+  'Secratario': ['Lucia'],
+  "Sergent D'arm": ['Antonio'],
+  'Road Captain': ['Rosa'],
+  'Vice Présidente': ['Javier'],
+  'Présidente': ['Angel']
+};
